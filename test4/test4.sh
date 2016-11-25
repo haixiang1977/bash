2f8d2585d57907b94f49a928bf50bee494b54fa0
liu haixiang <haixiang1977@yahoo.com>
2016-11-26 00:10:41 +0800

    complete the functionality . use `function` + echo to give output for caller use . use for to loop the directory . use $ to cascade string and $(...) to execute command . use dirname to get parent directory
    Signed-off-by: liu haixiang <haixiang1977@yahoo.com>

--- test4/test4.sh
+++ test4/test4.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # read filelist from backup directory
-# then copy from ./tmp/ to ./restore/
+# then copy from ./tmp/ to ./restore/ according to the backupfilelist
+# in ./backup/
 
 # check ./backup ./tmp and ./restore directory available or not
 if [ ! -d ./tmp ]; then
@@ -12,15 +13,62 @@ if [ ! -d ./tmp ]; then
     echo "hello b" > ./tmp/b/b.txt
 fi
 
+srcdir="./tmp/"
+
 if [ ! -d ./restore ]; then
     mkdir -p ./restore
 fi
 
+dstdir="./restore/"
+
 if [ ! -d ./backup ]; then
     mkdir -p ./backup
+    mkdir -p ./backup/a
+    mkdir -p ./backup/b
 fi
 
-# general backfilelist at ./backup directory
-echo "./a/a.txt" > ./backup/backfilelist
-echo "./b/b.txt" > ./backup/backfilelist
+# general backupfilelist at ./backup directory
+echo "./a/a.txt" > ./backup/a/backupfilelist
+echo "./b/b.txt" > ./backup/b/backupfilelist
+
+# function to get all ./backup/ directory
+function get_backupfile_dir {
+    local dirs=""
+    # loop the directory and return all
+    for d in ./backup/*; do
+        if [ -d "$d" ]; then
+            dirs="$d $dirs"
+        fi
+    done
+    # later $dirs will be used by function caller with `get_backupfile_dir`
+    echo "$dirs"
+}
+
+# function to get all contents in backfilelists
+function get_backup_files {
+    for d in `get_backupfile_dir`; do
+        if [ -f $d/backupfilelist ]; then
+            # $(command) command substitution
+            # take backuofilelist as input to echo, the caller will use it
+            echo "$(< $d/backupfilelist)"
+        fi
+    done
+}
+
+# start to backup now ...
+for entry in `get_backup_files`; do
+    # create restore directory
+    dir=`dirname $entry`
+    mkdir -p $dstdir/$dir
+    
+    # check src directory backup file available or not
+    if [ ! -f $srcdir$entry ]; then
+        echo "$srcdir$entry file not found!!!"
+    else
+        # copy file from src directory to restore directory
+        cp $srcdir$entry $dstdir$dir
+    fi
+done
+        
+    
 
